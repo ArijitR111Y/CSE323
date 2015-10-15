c11285495fbad7f272d6956329b10cb3d8fec836
ABHISHEK BALAJI <13bce0836@sjt319scs021.vituniversity.local>
2015-10-15 17:05:20 +0530

    Added first exercise

---
+++ untitled1.tcl
@@ -0,0 +1,34 @@
+set ns [new Simulator]
+set nf [open out.nam w]
+
+$ns namtrace-all $nf
+
+
+proc finish {} {
+	global ns nf
+	$ns flush-trace
+	close $nf
+	exec nam out.nam &
+	exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set udp0 [new Agent/UDP]
+set null0 [new Agent/Null]
+$ns attach-agent $n1 $null0
+$ns connect $udp0 $null0
+$ns attach-agent $n0 $udp0
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetSize_ 500
+$cbr0 set interval_ 0.005
+$cbr0 attach-agent $udp0
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+
+
+
+$ns at 5.0 "finish"
+$ns at 0.5 "$cbr0 start"
+$ns at 4.5 "$cbr0 stop"
+
+$ns run
