6d18f66c5d2293db0b10a20590451a071b11f74f
ABHISHEK BALAJI <13bce0836@sjt319scs021.vituniversity.local>
2015-10-15 17:30:46 +0530

    Added all

--- untitled1.tcl
+++ untitled1.tcl
@@ -14,17 +14,28 @@ proc finish {} {
 
 set n0 [$ns node]
 set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+
 set udp0 [new Agent/UDP]
 set null0 [new Agent/Null]
+
 $ns attach-agent $n1 $null0
 $ns connect $udp0 $null0
 $ns attach-agent $n0 $udp0
+
 set cbr0 [new Application/Traffic/CBR]
+
 $cbr0 set packetSize_ 500
 $cbr0 set interval_ 0.005
 $cbr0 attach-agent $udp0
-$ns duplex-link $n0 $n1 1Mb 10ms DropTail
 
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n3 1Mb 10ms DropTail
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail
+$ns duplex-link $n0 $n3 1Mb 10ms DropTail
+$ns duplex-link $n3 $n1 1Mb 10ms DropTail
 
 
 $ns at 5.0 "finish"
